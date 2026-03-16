4885d942d7dc988d9f43a17a865f2e93fcd7110c
harleenkaur1343 <harleenkaur1343@gmail.com>
2026-03-16 18:55:38 +0530

    Update next.config file 02_nextjs module

--- nextjs/02-task-notes-design/frontend/next.config.ts
+++ nextjs/02-task-notes-design/frontend/next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
+import ".env";
 
 const nextConfig: NextConfig = {
   /* config options here */
    async rewrites() {
+     const apiUrl = process.env.API_URL || 'http://localhost:9000';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:9000/api/:path*',
+        destination: `import.meta.${apiUrl}/api/:path*`,
       },
     ];
   },
